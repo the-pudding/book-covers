d7435c87675a936f7652dd9960aa28630eabf17a
Jess <jessicapeter92@gmail.com>
2019-04-17 16:13:08 +0200

    updated dist

--- webpack.config.js
+++ webpack.config.js
@@ -20,23 +20,24 @@ module.exports = {
       {
       test: /\.(?:png|jpg|svg)$/,
       loader: 'url-loader',
-      query: {
-        // Inline images smaller than 10kb as data URIs
-        limit: 10000
+        query: {
+          // Inline images smaller than 10kb as data URIs
+          limit: 10000
+        }
       },
       {
         test: /\.(png|jpg|gif)$/,
         use: [
           'file-loader',
             {
-		      loader: 'image-webpack-loader',
-		      options: {
-		        bypassOnDebug: true, // webpack@1.x
-		        disable: true, // webpack@2.x and newer
-		    },
-          },
+    		      loader: 'image-webpack-loader',
+    		      options: {
+    		        bypassOnDebug: true, // webpack@1.x
+    		        disable: true, // webpack@2.x and newer
+    		      },
+            },
         ]
-    }
+      }
     ]
   }
 };
