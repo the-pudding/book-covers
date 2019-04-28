49316f81abae49f964de3116ea63156e6db9f7eb
Jess <jessicapeter92@gmail.com>
2019-04-28 15:49:04 +0200

    added and styled some chip-style filters--still not functional though

--- src/activeFilters.js
+++ src/activeFilters.js
@@ -41,7 +41,7 @@ class ActiveFilters {
 
 		let chips = d3.select(this.holder)
 			.selectAll(".chipHolder")
-			.data(allSelected);
+			.data(allSelected, function(d){ return d[0]});
 
 		chips.exit().remove();
 
@@ -51,10 +51,80 @@ class ActiveFilters {
 			.each(function(e){
 				let thisHolder = d3.select(this);
 				let thisDiv = thisHolder.append("div")
-								.attr("class", "chip");
+								.attr("class", "chip closed");
 
-				thisDiv.append("p").html(e[0] + " is " + e[1]);
-			})
+				let theP = thisDiv.append("p");
+				theP.append("span").attr("class", "chipCloser");
+
+				if (e[0] !== "motifs"){
+					theP.append("span")
+						.attr("class", "chipInnerText")
+						.html(e[0] + " is " + e[1]);
+				} else {
+					theP.append("span")
+						.attr("class", "chipInnerText")
+						.html(e[0] + " include " + e[1]);
+				}
+
+				//toggle dropdown on click
+				thisDiv.select("p")
+					.append("span")
+					.attr("class", "chipToggler")
+					.on("click", function(f){
+						thisDiv.classed("closed", !thisDiv.classed("closed"));
+					})
+
+
+				thisDiv.append("div").attr("class", "chipSelectionHolder");
+
+			});
+
+		//different appearance if multiple things selected
+		chips.each(function(e){
+			let thisHolder = d3.select(this);
+			let thisDiv = thisHolder.select("div");
+
+			if (e[1].length === 1){
+				thisDiv.classed("dropDown", false);
+
+				if (e[0] !== "motifs"){
+					thisDiv.select(".chipInnerText")
+						.html(e[0] + " is " + e[1]);
+				} else {
+					thisDiv.select(".chipInnerText")
+						.html(e[0] + " include " + e[1]);
+				}
+
+				thisDiv.select(".chipSelectionHolder").selectAll("*").remove();
+			} else {
+				thisDiv.classed("dropDown", true);
+
+				thisDiv.select(".chipInnerText").html(e[0] + " (" + e.length + ")");
+				
+				let dropdown = thisDiv.select(".chipSelectionHolder")
+				.selectAll(".chipSelection")
+				.data(e[1], function(f){ return f});
+
+				dropdown.exit().remove();
+
+				dropdown.enter()
+					.append("div")
+					.attr("class", "chipSelection")
+					.append("p")
+					.each(function(f){
+						let thisHolder = d3.select(this);
+
+						thisHolder.append("span")
+							.attr("class", "chipCloser");
+
+						thisHolder.append("span")
+							.html(function(f){ return f});
+					});
+			}
+
+			
+
+		})
 
 	}
 
