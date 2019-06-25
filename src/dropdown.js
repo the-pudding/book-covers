e2f4d624caa19e7c8d4d18134be1e5f3cc6a975b
Matt Daniels <matthew.f.daniels@gmail.com>
2019-06-25 18:06:26 -0400

    here

--- src/dropdown.js
+++ src/dropdown.js
@@ -14,25 +14,23 @@ class Dropdown {
 		this.name;//i.e. gender, genre etc.
 
 		this.totalData;//unfiltered
-        this.filteredData;
-        this.selection = [];//values selected in index.js
-        this.compiledData;//data we've formatted/operated on
-        this.selectedValues = [];//formatted values representing what is currently selected
-        this.results = [];//the div that holds the selectable values
+    this.filteredData;
+    this.selection = [];//values selected in index.js
+    this.compiledData;//data we've formatted/operated on
+    this.selectedValues = [];//formatted values representing what is currently selected
+    this.results = [];//the div that holds the selectable values
 
-        this.callback;
+    this.callback;
 
-        this.sort = "total";
+    this.sort = "total";
 
-        this.init = this.init.bind(this);
-       	this.handleKeypress = this.handleKeypress.bind(this);
-        this.setData = this.setData.bind(this);
-        this.draw = this.draw.bind(this);
-        this.drawOnePanel = this.drawOnePanel.bind(this);//draw our selections
-        this.changeSort = this.changeSort.bind(this);
-        this.doSort = this.doSort.bind(this);
-
-
+    this.init = this.init.bind(this);
+   	this.handleKeypress = this.handleKeypress.bind(this);
+    this.setData = this.setData.bind(this);
+    this.draw = this.draw.bind(this);
+    this.drawOnePanel = this.drawOnePanel.bind(this);//draw our selections
+    this.changeSort = this.changeSort.bind(this);
+    this.doSort = this.doSort.bind(this);
 	}
 
 	init(name, hasSearch){
@@ -59,7 +57,7 @@ class Dropdown {
 
 		theSelect.append("option").attr("value", "filtered")
 			.html("Sort by Filtered Count");
-		
+
 		let changeSort = this.changeSort;
 		theSelect.on("change", function(d, e){
 			changeSort(this.value);
@@ -70,13 +68,13 @@ class Dropdown {
 		let handleKeypress = this.handleKeypress;
 
 		let results = this.holder.append("div").attr("class", "results");
-		
+
 		if (this.hasSearch){
 			let searchHolder = results.append("div").attr("class", "searchHolder iconBefore");
 
 			this.input = searchHolder.append("div")
 				.attr("style", "position: relative");
-		
+
 			this.input.append("input")
 				.attr("class", "searchBar")
 				.attr("placeholder", function(){
@@ -91,9 +89,9 @@ class Dropdown {
 					} else {
 						return "Search...";
 					}
-					
+
 				});
-			
+
 			//empty the search area when we click the clear icon
 			this.input.append("div")
 				.attr("class", "searchClearer iconAfter")
@@ -130,12 +128,15 @@ class Dropdown {
 				}
 			});
 
+		results.append("div")
+			.attr("class","bottom-gradient");
+
 	}
 
 	changeSort(name){
 		this.sort = name;
 		this.doSort();
-		
+
 	}
 
 	doSort(){
@@ -167,9 +168,24 @@ class Dropdown {
 
 		if (selection.length === 0){
 			this.holder.select("h3").html(this.name);
+
 		} else {
 			this.holder.select("h3").html(this.name + " (" + selection.length + ")");
 		}
+
+		if(selection.length > 0){
+			if(this.holder.attr("id") == "selected"){
+				this.holder.style("visibility","visible");
+			}
+			this.holder.select("button").style("display","block");
+		}
+		else{
+			if(this.holder.attr("id") == "selected"){
+				this.holder.style("visibility",null);
+			}
+			this.holder.select("button").style("display",null);
+		}
+
 		this.totalData = total;
 		this.filteredData = filtered;
 		this.selection = selection;
@@ -196,7 +212,7 @@ class Dropdown {
 
 		//make a search object using the fuse library and add a key listener
 		if (this.hasSearch){
-			this.fuse = new Fuse(this.compiledData, 
+			this.fuse = new Fuse(this.compiledData,
 				{keys: ["key"],
 				threshold: 0.3
 				}
@@ -223,6 +239,7 @@ class Dropdown {
 	}
 
 	draw(){
+		console.log("here");
 		this.drawOnePanel(this.selectedHolder, this.selectedValues, this.callback);
 	}
 
@@ -236,7 +253,7 @@ class Dropdown {
 
 		panel.exit().remove();
 
-		//doing this as absolutely positioned so we can update the vals rather than 
+		//doing this as absolutely positioned so we can update the vals rather than
 		//redrawing everything
 		panel
 		.style("top", function(d, i){ return i * 2 + "em"})
@@ -244,7 +261,7 @@ class Dropdown {
 				let theThis = d3.select(this);
 				theThis.select(".valName").select("p").html(d.key);
 				theThis.select(".count").select("p").html(d.filteredValue + "/" + d.value);
-				
+
 				if (theSelected.includes(d.key)) {
 					theThis.select(".checker").classed("checked", true);
 				} else {
@@ -263,7 +280,7 @@ class Dropdown {
 				} else {
 					theThis.append("div").append("p").attr("class", "checker iconAfter");
 				}
-				
+
 				theThis.append("div").attr("class", "valName").append("p").html(d.key);
 				theThis.append("div").attr("class", "count").append("p").html(d.filteredValue + "/" + d.value);
 			}).on("click", function(d){
@@ -287,7 +304,7 @@ class Dropdown {
 
 		if (event.keycode || event.which){
 			var code = (event.keyCode ? event.keyCode : event.which);
-			if(code == 13) { 
+			if(code == 13) {
 			   //Enter keycode -- no need rn
 			}
 		}
